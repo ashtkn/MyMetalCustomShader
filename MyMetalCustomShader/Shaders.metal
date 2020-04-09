d96cab903ca643337641f91cd36e45defbcdc1f5
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-04-09 17:42:44 +0900

    feat: implement custom shader http://glslsandbox.com/e#36614.0

---
+++ MyMetalCustomShader/Shaders.metal
@@ -0,0 +1,64 @@
+//
+//  Shaders.metal
+//  MyMetalCustomShader
+//
+//  Created by 竹ノ内朝陽 on 2020/04/09.
+//  Copyright © 2020 竹ノ内朝陽. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexInOut
+{
+    float4 position [[ position ]];
+};
+
+vertex VertexInOut vertexShader(device float4 *positions [[ buffer(0) ]], uint vid [[ vertex_id ]])
+{
+    VertexInOut out;
+    out.position = positions[vid];
+    return out;
+}
+
+float2 hash(float2 p)
+{
+    float2x2 m = float2x2(13.85, 47.77, 99.41, 88.48);
+    return fract(sin(m*p) * 46738.29);
+}
+
+float voronoi(float2 p)
+{
+    float2 g = floor(p);
+    float2 f = fract(p);
+
+    float distanceToClosestFeaturePoint = 1.6;
+    for(int y = -1; y <= 1; y++)
+    {
+        for(int x = -1; x <= 1; x++)
+        {
+            float2 latticePoint = float2(x, y);
+            float currentDistance = distance(latticePoint + hash(g+latticePoint), f);
+            distanceToClosestFeaturePoint = min(distanceToClosestFeaturePoint, currentDistance);
+        }
+    }
+
+    return distanceToClosestFeaturePoint;
+}
+
+fragment float4 fragmentShader(VertexInOut in [[ stage_in ]], constant float2 &resolution [[ buffer(0) ]])
+{
+    float time = 100;
+    float4 position = in.position;
+    
+    float2 uv = ( position.xy / resolution.xy ) * 2.0 - 1.0;
+    uv.x *= resolution.x / resolution.y;
+
+    float offset = voronoi(uv*10.0 + float2(time));
+    float t = 1.0/abs(((uv.x + sin(uv.y + time)) + offset) * 30.0);
+
+    float r = voronoi( uv * 1.0 ) * 10.0;
+    float3 finalColor = float3(10.0 * uv.y, 2.0, 1.0 * r) * t;
+    
+    return float4(finalColor, 1.0);
+}
--- MyMetalCustomShader/ViewController.swift
+++ MyMetalCustomShader/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
-
+    
+    private let vertexData: [Float] = [
+        -1, -1, 0, 1,
+        1, -1, 0, 1,
+        -1,  1, 0, 1,
+        1,  1, 0, 1
+    ]
+    
+    private let resolutionData: [Float] = [
+        Float(UIScreen.main.nativeBounds.size.width),
+        Float(UIScreen.main.nativeBounds.size.height)
+    ]
+    
+    private let device = MTLCreateSystemDefaultDevice()!
+    private var commandQueue: MTLCommandQueue!
+    
+    private var vertexBuffer: MTLBuffer!
+    private var resolutionBuffer: MTLBuffer!
+    // private var timeBuffer: MTLBuffer!
+    
+    private var renderPipelineState: MTLRenderPipelineState!
+    private let renderPassDescriptor = MTLRenderPassDescriptor()
+    
+    @IBOutlet private weak var mtkView: MTKView!{
+        didSet {
+            mtkView.device = device
+            mtkView.delegate = self
+            mtkView.enableSetNeedsDisplay = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Setup Metal
+        commandQueue = device.makeCommandQueue()!
+        
+        // Make buffer
+        makeVertexBuffer()
+        makeResolutionBuffer()
+        
+        // Make pipeline state
+        let library = device.makeDefaultLibrary()!
+        let descriptor = MTLRenderPipelineDescriptor()
+        descriptor.vertexFunction = library.makeFunction(name: "vertexShader")
+        descriptor.fragmentFunction = library.makeFunction(name: "fragmentShader")
+        descriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineState = try! device.makeRenderPipelineState(descriptor: descriptor)
+        
+        // Draw
+        mtkView.setNeedsDisplay()
     }
+    
+    private func makeVertexBuffer() {
+        let size = vertexData.count * MemoryLayout<Float>.size
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: size)
+    }
+    
+    private func makeResolutionBuffer() {
+        let size = resolutionData.count * MemoryLayout<Float>.size
+        resolutionBuffer = device.makeBuffer(bytes: resolutionData, length: size, options: [])
+    }
+    
+}
 
+extension ViewController: MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        print("\(self.classForCoder)/" + #function)
+    }
+    
+    func draw(in view: MTKView) {
+        let drawable = view.currentDrawable!
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        
+        guard let renderPipelineState = renderPipelineState else { fatalError() }
+        renderEncoder.setRenderPipelineState(renderPipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.setFragmentBuffer(resolutionBuffer, offset: 0, index: 0)
 
+        renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+    }
 }
 
